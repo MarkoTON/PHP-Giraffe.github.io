803c3d874348167dc9dfa55a6cffd5b6826ae2c8
Marko <29685366+MarkoTON@users.noreply.github.com>
2021-11-16 16:14:16 +0100

    initial start

--- site.php
+++ site.php
@@ -1,6 +1,19 @@
-<?php 
- 
- echo 'Hello world';
- echo 'Less 4';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+  <?php 
+  
+    echo 'Hello world';
+    echo '<br>';
+    echo 'Less 4';
 
-?>
+  ?>
+</body>
+</html>
