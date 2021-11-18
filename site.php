d883aa50dbcb73a6e4ac78208396b75cb08f95f9
Marko <29685366+MarkoTON@users.noreply.github.com>
2021-11-18 16:18:09 +0100

    less-13

--- site.php
+++ site.php
@@ -9,24 +9,16 @@
 <body>
 
   <form action="site.php" method="get">
-    Color:<input type="text" name="color">
-    <br>
-    Plural noun:<input type="text" name="plural">
-    <br>
-    Celebrity<input type="text" name="celebrity">
-    <br>
+    Name: <input type="text" name="name">
     <input type="submit">
   </form>
   Answer:
   <br>
   <?php 
-    $color = $_GET["color"];
-    $plural = $_GET["plural"];
-    $celebrity = $_GET["celebrity"];
+    $color = $_GET["name"];
 
-    echo "Roses are $color <br>";
-    echo "$plural are blue <br>";
-    echo "I love $celebrity <br>";
+    echo $color;
+   
   ?>
 </body>
 </html>
