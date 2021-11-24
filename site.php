498f4ae5dff243952052f00cf3c9c6242893611f
Marko <29685366+MarkoTON@users.noreply.github.com>
2021-11-24 15:52:17 +0100

    less-23

--- site.php
+++ site.php
@@ -8,30 +8,22 @@
 </head>
 <body>
 
-  <form action="site.php" method="post">
-    First num: <input type="number" step="0.1" name="num1" ><br>
-    OP: <input type="text" name="op" ><br>
-    Second num: <input type="number" step="0.1" name="num2" ><br>
-    <input type="submit">
-  </form>
+
   <br>
   <?php 
-    $num1 = $_POST["num1"];
-    $num2 = $_POST["num2"];
-    $op = $_POST["op"];
+    $i = 0;
+    while($i < 10){
+      echo "$i <br>";
+      $i++;
+    };
 
-    if($op == "+"){
-      echo $num1 + $num2;
-    } else if ($op == "-"){
-      echo $num1 - $num2;
-    } else if ($op == "/"){
-      echo $num1 / $num2;
-    } else if ($op == "*"){
-      echo $num1 * $num2;
-    } else {
-      echo "Invalud Operator";
-    }
+    echo "<hr>";
+    $x = 1;
 
+    do {
+      echo "The number is: $x <br>";
+      $x++;
+    } while ($x <= 5);
   ?>
 </body>
 </html>
