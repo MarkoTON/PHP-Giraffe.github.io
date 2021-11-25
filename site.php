472ee30822a645f13b1c4c49e6fdb1f58ce63ff0
Marko <29685366+MarkoTON@users.noreply.github.com>
2021-11-25 14:23:27 +0100

    less-30

--- site.php
+++ site.php
@@ -14,17 +14,29 @@
       var $title;
       var $author;
       var $pages;      
+
+      function __construct($aTitle, $aAuthor, $aPages)
+      {
+        // echo "New Book Created <br>";
+        // echo "New $name <br>";
+        $this ->title = $aTitle;
+        $this ->author = $aAuthor;
+        $this ->pages = $aPages;
+      }
     };
 
-    $book1 = new Book;
-    $book1->title = "Harry Potter";
-    $book1->author = "JK Rowling";
-    $book1->pages = 400;
+    // $book1 = new Book("Marko");
+    // $book1->title = "Harry Potter";
+    // $book1->author = "JK Rowling";
+    // $book1->pages = 400;
 
-    $book2 = new Book;
-    $book2->title = "Title";
-    $book2->author = "Author";
-    $book2->pages = 456;
+    // $book2 = new Book("John");
+    // $book2->title = "Title";
+    // $book2->author = "Author";
+    // $book2->pages = 456;
+
+    $book1 = new Book("Title for book 1", "Author for book 1", "Pages for book 1");
+    $book2 = new Book("Title for book 2", "Author for book 2", "Pages for book 3");
 
     echo $book1->title;
     echo "<br>";
